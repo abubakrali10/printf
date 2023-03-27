bb5ef58b56a90653318b602a9631393485941986
abubakrali10 <bakrali175@yahoo.com>
2023-03-27 15:16:45 -0700

    function that handle (%d and %i) specifier

---
+++ p_int.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print - Entry Point
+ * @n: number
+ * Return: void
+ */
+
+void print(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * (-1);
+	}
+	if (n / 10 != 0)
+	{
+		print(n / 10);
+	}
+	_putchar(n % 10 + '0');
+}
+
+/**
+ * p_int - handling (%d and %i) specifier
+ * @args: list name
+ * Return: length
+ */
+
+int p_int(va_list args)
+{
+	int len = 0;
+	int s = va_arg(args, int);
+
+	print(s);
+	if (s < 0)
+	{
+		len++;
+		s = s * (-1);
+	}
+	if (s > 0)
+	{
+		s = s / 10;
+		len++;
+	}
+
+	return (len);
+}
