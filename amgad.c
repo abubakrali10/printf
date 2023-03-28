62975f207d383381c2cd9a21ebfe9452d01bbe60
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-03-27 22:58:47 -0700

    change function that print int

--- amgad.c
+++ amgad.c
@@ -1,44 +1,52 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdarg.h>
+
+/**
+ * print - Entry Point
+ * @n: number
+ * Return: void
+ */
+
+void print(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * -1;
+	}
+	if (n / 10 != 0)
+		print(n / 10);
+	_putchar(n % 10 + '0');
+}
+
 /**
  * p_int - handling (%d and %i) specifier
  * @args: list name
  * Return: length
  */
+
 int p_int(va_list args)
 {
-	int len = 0, j, reverse = 0;
+	int len = 0;
 	int s = va_arg(args, int);
 
+	print(s);
 	if (s == 0)
 	{
 		_putchar('0');
-		return (1);
-	}
-
-	if (s < 0)
-	{
-		_putchar('-');
-		s = s * -1;
 		len++;
 	}
-	j = s % 10;
-	s /= 10;
-	len++;
-
-	while (s != 0)
+	if (s < 0)
 	{
-		reverse = (reverse * 10) + s % 10;
+		len++;
+		s *= -1;
+	}
+	while (s > 0)
+	{
 		s /= 10;
 		len++;
 	}
-	while (reverse > 0)
-	{
-		_putchar(reverse % 10 + '0');
-		reverse /= 10;
-	}
-	_putchar(j + '0');
 
 	return (len);
 }
